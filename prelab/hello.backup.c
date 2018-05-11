61e6a15283f2ec16ab21787b5f54c2a73d6f5361
Sean O'Donnell <odonnell.325@buckeyemail.osu.edu>
2018-05-11 12:21:51 -0400

    initial commit

---
+++ prelab/hello.backup.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/* Author: Sean O'Donnell */
+int main(void)
+{
+char name_string[] = "Sean O'Donnell";
+int i;
+int max = 1;
+
+for (i = 0; i <max; i++)
+{
+	printf("Hello, World!\n");
+}
+printf("My name is %s.\n", name_string);
+return (0);
+}
