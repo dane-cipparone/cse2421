d21c3bd23686d153ead00a95f9991005667027b1
Sean O'Donnell <odonnell.325@buckeyemail.osu.edu>
2018-05-16 14:20:58 -0400

    debugged, reformatted and made readme

--- prelab/hello.c
+++ prelab/hello.c
@@ -2,14 +2,14 @@
 /* Author: Sean O'Donnell */
 int main(void)
 {
-char name_string[] = "Sean O'Donnell";
-int i;
-int max = 1;
+    char name_string[] = "Sean O'Donnell";
+    int i;
+    int max = 1;
 
-for (i = 0; i <max; i++)
-{
+    for (i = 0; i <max; i++)
+    {
 	printf("Hello, World!\n");
-}
-printf("My name is %s.\n", name_string);
-return (0);
+    }
+    printf("My name is %s.\n", name_string);
+    return (0);
 }
